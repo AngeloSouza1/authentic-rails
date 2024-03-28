cc78e73e6f258b486463e26e3e0373940da1f03e
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-03-27 22:07:19 -0300

    feat: include project

---
+++ app/policies/article_policy.rb
@@ -0,0 +1,35 @@
+class ArticlePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    user&.has_role? :writer
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    # se é admin
+    # user&.admin?
+    # se é dono do artigo
+    # user&.admin? || user&.id == record.user_id
+    user&.has_role? :editor || user&.id == record.user_id
+  end
+
+  def destroy?
+    # se é admin
+    # user&.admin?
+    # se é dono do artigo
+    user&.admin? || user&.id == record.user_id
+  end
+end
